c6394a008101d424460265a67bc141d2725c99fc
sumitkumar-hub <jaiswalsumit583@gmail.com>
2026-07-17 01:03:32 +0530

    Time: 13 ms (13.7%), Space: 52.7 MB (6.76%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+
+                char ch = board[i][j];
+
+                if (ch == '.') {
+                    continue;
+                }
+
+                if (!set.add(ch + "row" + i) ||
+                    !set.add(ch + "col" + j) ||
+                    !set.add(ch + "box" + (i / 3) + (j / 3))) {
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
