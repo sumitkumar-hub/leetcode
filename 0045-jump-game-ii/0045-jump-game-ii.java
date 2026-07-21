9318280714169a67f67c14886a8d98c77b74c34f
sumitkumar-hub <jaiswalsumit583@gmail.com>
2026-07-21 23:39:50 +0530

    Time: 43 ms (11.5%), Space: 47.1 MB (88.43%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 100000);
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            
+            for (int j = 1; j <= nums[i] && i + j < n; j++) {
+                
+                dp[i + j] = Math.min(dp[i + j], dp[i] + 1);
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
